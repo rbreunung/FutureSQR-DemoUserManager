6a839446c8aee5bf96957f85b28da6b25a459520
Robert Breunung <rbreunung@gmail.com>
2022-10-03 23:12:37 +0200

    Fix Test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'FutureSQR-DemoUserManager'`, () => {
+  it(`should have as title 'FutureSQR - Demo User Manager'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('FutureSQR-DemoUserManager');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('FutureSQR-DemoUserManager app is running!');
+    expect(app.title).toEqual('FutureSQR - Demo User Manager');
   });
 });
