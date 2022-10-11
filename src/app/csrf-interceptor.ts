28b0ee84c25ede6a1e17f136212d68073c755deb
Robert Breunung <rbreunung@gmail.com>
2022-10-11 10:51:49 +0200

    intercept push

--- src/app/csrf-interceptor.ts
+++ src/app/csrf-interceptor.ts
@@ -1,15 +1,29 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpEvent, HttpResponse, HttpRequest, HttpHandler } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { first, map, mergeMap, Observable } from 'rxjs';
 import { CsrfService } from './csrf.service';
 
 @Injectable()
 export class CsrfInterceptor implements HttpInterceptor {
 
-    constructor(privatecsrfService:CsrfService) { }
+    csrfMethods: string[] = ["post", "put", "delete"];
+
+    constructor(private csrfService: CsrfService) { }
 
     intercept(httpRequest: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        console.info("I did an intercept");
+        if (this.csrfMethods.indexOf(httpRequest.method) > -1) {
+            console.info(`found relevant method ${httpRequest.method}`);
+            return this.csrfService.getCsrfToken().pipe(first(), mergeMap(
+                token => {
+                    let newRequest = httpRequest.clone();
+                    newRequest.headers.append(token.headerName, token.token);
+                    return next.handle(newRequest);
+                }
+
+            ));
+        } else {
+            console.info(`found non relevant method ${httpRequest.method}`);
+        }
         return next.handle(httpRequest);
     }
 }
