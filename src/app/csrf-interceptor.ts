b7d6f1e4ba0b01da9c115c8bf72261091211f832
Robert Breunung <rbreunung@gmail.com>
2022-10-11 12:15:08 +0200

    add proxy direct to Spring server

--- src/app/csrf-interceptor.ts
+++ src/app/csrf-interceptor.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpEvent, HttpResponse, HttpRequest, HttpHandler } from '@angular/common/http';
-import { first, map, mergeMap, Observable } from 'rxjs';
+import { HttpInterceptor, HttpEvent, HttpRequest, HttpHandler } from '@angular/common/http';
+import { first, mergeMap, Observable } from 'rxjs';
 import { CsrfService } from './csrf.service';
 
 @Injectable()
@@ -11,16 +11,14 @@ export class CsrfInterceptor implements HttpInterceptor {
     constructor(private csrfService: CsrfService) { }
 
     intercept(httpRequest: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        if (this.csrfMethods.indexOf(httpRequest.method) > -1) {
+        if (this.csrfMethods.indexOf(httpRequest.method.toLowerCase()) > -1) {
             console.info(`found relevant method ${httpRequest.method}`);
             return this.csrfService.getCsrfToken().pipe(first(), mergeMap(
                 token => {
                     let newRequest = httpRequest.clone();
                     newRequest.headers.append(token.headerName, token.token);
                     return next.handle(newRequest);
-                }
-
-            ));
+                }));
         } else {
             console.info(`found non relevant method ${httpRequest.method}`);
         }
