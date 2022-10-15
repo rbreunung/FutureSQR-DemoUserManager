c87438ad3e7a1d4c09922bcabbcb441a41c5c0b3
Robert Breunung <rbreunung@gmail.com>
2022-10-16 01:12:33 +0200

    simple user list service

--- src/app/userinfo.ts
+++ src/app/userinfo.ts
@@ -1,10 +1,17 @@
-export interface UserInfo {
+export interface User {
 
     uuid: string;
     loginName: string;
     displayName: string;
     vcsNames: string[];
-    avatarLocation: string;
+    avatarId: string;
     contactEmail: string;
     banned: boolean;
+}
+
+export interface SimpleUser {
+    uuid: string;
+    displayName: string;
+    avatarId: string;
+
 }
---
+++ src/app/userlist.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { SimpleUser } from './userinfo';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserListService {
+
+  constructor(private http: HttpClient) { }
+
+  getUserMap(): Observable<SimpleUser[]> {
+    const path = '/rest/user/simplelist';
+    return this.http.get<SimpleUser[]>(path);
+  }
+}
