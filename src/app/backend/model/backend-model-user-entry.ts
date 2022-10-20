aac61188aadc161c7907c8557cdfeb08d768621d
Robert Breunung <rbreunung@gmail.com>
2022-10-20 21:46:26 +0200

    user model

---
+++ src/app/backend/model/backend-model-user-entry.ts
@@ -0,0 +1,19 @@
+export class BackendModelFullUserEntry {
+	public uuid: string = "";
+	public loginname: string = "";
+	public displayname: string = "";
+	public avatarlocation: string = "";
+	public cvsNames: string[] = [];
+	public email: string = "";
+	public isbanned: boolean = false;
+	public created: number = Date.now();
+	public modified: number = Date.now();
+	public banned: number = Date.now();
+}
+
+export class BackendModelSimpleUserEntry {
+	public uuid: string = "";
+	public displayname: string = "";
+	public avatarlocation: string = "";
+	public isbanned: boolean = false;
+}
